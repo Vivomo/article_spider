767cf7c9bb939acad32c6c72d577b54a49ea32a7
Vimo <929992114@qq.com>
2017-10-05 22:13:48 +0800

    item -> json

--- ArticleSpider/ArticleSpider/pipelines.py
+++ ArticleSpider/ArticleSpider/pipelines.py
@@ -1,15 +1,47 @@
 # -*- coding: utf-8 -*-
 
 from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exporters import JsonItemExporter
 
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import codecs
+import json
 
 class ArticlespiderPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+
+class JsonWithEncodingPipeline(object):
+    # 自定义json文件的导出
+    def __init__(self):
+        self.file = codecs.open('article.json', 'w', encoding='utf-8')
+
+    def process_item(self, item, spider):
+        lines = json.dumps(dict(item), ensure_ascii=False) + '\n'
+        self.file.write(lines)
+        return item
+
+    def spider_closed(self, spider):
+        self.file.close()
+
+
+class JsonExporterPipeline(object):
+    # 调用scrapy提供的json export 到处json文件
+    def __init__(self):
+        self.file = open('articleExport.json', 'wb')
+        self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
 
 
--- ArticleSpider/ArticleSpider/settings.py
+++ ArticleSpider/ArticleSpider/settings.py
@@ -67,7 +67,7 @@ ROBOTSTXT_OBEY = False
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'ArticleSpider.pipelines.ArticlespiderPipeline': 300,
+    'ArticleSpider.pipelines.JsonExporterPipeline': 2,
     'ArticleSpider.pipelines.ArticleImagePipeline': 1,
     # 'scrapy.pipelines.images.ImagesPipeline': 1
 }
